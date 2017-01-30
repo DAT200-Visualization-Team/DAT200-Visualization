216fec97113c4b47be1b00c1fc61ed1202f4c78f
Magnus Book <MS.Book@stud.uis.no>
2017-01-30 15:13:55 +0100

    Made font size use cookies to store its value and use it to change font size when loaded

--- js/settings.js
+++ js/settings.js
@@ -1,12 +1,25 @@
 $(document).ready(function(){
     $('#settings-modal').modal();
-    $('select').material_select();
     $(".button-collapse").sideNav();
+    loadFontSizeCookieValue();
+    $('select').material_select();
 });
 
 $(document).on('change', '#font-size-select', function () {
-    if($('#code-text'))
-        $('#code-text').css({ 'font-size': $(this).find('option:selected').attr('value') + "px" });
 
-    //TODO: Save font size in cookies
+    var selectedSize = $(this).find('option:selected').attr('value');
+
+    if($('#code-text'))
+        $('#code-text').css({ 'font-size': selectedSize + "px" });
+
+    setCookie('font-size', selectedSize, 1000);
 });
+
+function loadFontSizeCookieValue() {
+    var savedValue = getCookie('font-size');
+    if (savedValue) {
+        $('#font-size-select').val(savedValue);
+        if ($('#code-text'))
+            $('#code-text').css({ 'font-size': savedValue + "px" });
+    }
+}
