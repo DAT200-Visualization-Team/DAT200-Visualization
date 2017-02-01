b80ed5217314622f215ddbb89d2a9560568258d0
Magnus Book <MS.Book@stud.uis.no>
2017-02-01 13:57:55 +0100

    Base functionality of stack visualization is complete

--- js/visualization/StackVisualization.js
+++ js/visualization/StackVisualization.js
@@ -1,1 +1,18 @@
-﻿
+function initStack(values) {
+    $('#stack').empty();
+
+    if (values == null || !(values instanceof Array))
+        values = [1, 2, 3, 4, 5];
+
+    for (var i = 0; i < values.length; i++) {
+        pushElement(values[i]);
+    }
+}
+
+function pushElement(value) {
+    $('#stack').prepend('<div class="stack-entry"><h4 class="stack-value center-align">' + value + '</h4></div>');
+}
+
+function popElement() {
+    $('#stack').children().first().remove();
+}
