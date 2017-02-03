3e36b09e3c345f9f5b581e722a933aae6f0e3426
Magnus Book <MS.Book@stud.uis.no>
2017-02-03 11:25:51 +0100

    Changed stack animations to flying in/out instead of fading in/out

--- js/visualization/StackVisualization.js
+++ js/visualization/StackVisualization.js
@@ -37,8 +37,8 @@ function animatePushes(values) {
     }
 
     for (var i = 0; i < values.length; i++) {
-        var currentElement = $('<div class="stack-entry red lighten-3 z-depth-3" style="display: none;"><h4 class="stack-value center-align truncate">' + values[i] + '</h4></div>').prependTo('#stack');
-        loadingSequence.push({ e: currentElement, p: 'fadeIn', o: { duration: 1000 } });
+        var currentElement = $('<div class="stack-entry red lighten-3 z-depth-3" style="opacity: 0;"><h4 class="stack-value center-align truncate">' + values[i] + '</h4></div>').prependTo('#stack');
+        loadingSequence.push({ e: currentElement, p: {translateY: [0, -500], opacity: 1}, o: { duration: 1000, easing: 'easeOutCubic' } });
         stack.push(values[i]);
     }
 
@@ -54,11 +54,30 @@ function animatePops(count) {
 
     for (var i = 0; i < count; i++) {
         var currentElement = $('#stack').children().eq(i);
-        loadingSequence.push({ e: currentElement, p: 'fadeOut', o: { duration: 1000, complete: function (elements) { elements[0].remove() } } });
+
+        currentElement.velocity()
+        loadingSequence.push({
+            e: currentElement, p: { translateX: generateRandomNumberInRange(-100, 100), translateY: generateRandomNumberInRange(-100, -40) },
+            o: { duration: 1500, easing: 'easeOutQuad' },
+        });
+        loadingSequence.push({
+            e: currentElement, p: { translateX: pickRandomNumber(-5000, 5000) },
+            o: { duration: 1000, easing: 'easeInSine', complete: function (elements) { elements[0].remove() } },
+        });
+
         stack.pop();
     }
 
     $.Velocity.RunSequence(loadingSequence);
+}
+
+function generateRandomNumberInRange(min, max) {
+    return Math.floor(Math.random() * (max - min) + min);
+}
+
+function pickRandomNumber() {
+    var index = Math.floor(Math.random() * arguments.length)
+    return arguments[index];
 }
 
 function makeElementsEmpty() {
