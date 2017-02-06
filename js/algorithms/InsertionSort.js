55ca50140152bfeedb3f47fce9939e4a252c611c
STranoey <s.tranoy@stud.uis.no>
2017-02-06 13:56:57 +0100

    insertion sort v1

--- js/algorithms/InsertionSort.js
+++ js/algorithms/InsertionSort.js
@@ -3,19 +3,23 @@ function insertionSort(a) {
     var commands = [];
 
     for(var p = 1; p < a.length; p++) {
+        commands.push("codeLineHighlight(2)");
         var tmp = a[p];
+        commands.push("codeLineHighlight(3)");
         var j = p;
+        commands.push("codeLineHighlight(4)");
 
-        for(; j > 0 && commands.push("highlight( " + p + ", " + (j - 1) + ")") && tmp < a[j - 1]; j--) {
-            //commands.push("highlightCompare(j, j-1)");
+        for(; j > 0 && commands.push("highlight( " + j + ", " + (j - 1) + ")") && tmp < a[j - 1]; j--) {
             a[j] = a[j - 1];
 
             //GUI
-            //console.log("swap(" + j + ", " + (j - 1) + ");");
             commands.push("swap(" + j + ", " + (j - 1) + ");");
         }
         a[j] = tmp;
+        commands.push("codeLineHighlight(8)");
     }
+
+    commands.push("codeLineHighlight(10)");
 
     //Send commands to GUI
     console.log(commands);
--- js/sort.js
+++ js/sort.js
@@ -8,32 +8,39 @@ var height = 300;
 var cmd = [];
 var currentCmd = 0;
 
-//Code
-var codeContent = ["function insertionSort(a) {", "for(var p = 1; p < a.length; p++) {", "var tmp = a[p];", "var j = p;",
-    "for(; j > 0 && tmp < a[j - 1]; j--) {", "a[j] = a[j - 1];",
-    "}", "a[j] = tmp;", "}", "return a;", "}"];
-
-//var x = d3.scaleLinear().domain([0, data.length]).range([0, width]);
-//var scaled_y = d3.scaleLinear().domain([0, d3.max(data, function(d)
-//{return d;})]).rangeRound([0, height-15]);
+//PH - Code TODO get code from another source
+var codeContent = [
+    "",
+    "function insertionSort(a) {",
+    "    for(var p = 1; p < a.length; p++) {",
+    "        var tmp = a[p];",
+    "        var j = p;",
+    "        for(; j > 0 && tmp < a[j - 1]; j--) {",
+    "            a[j] = a[j - 1];",
+    "        }",
+    "        a[j] = tmp;",
+    "    }",
+    "    return a;",
+    "}"];
 
 var code = d3.select("#code-text");
-var barChart = d3.select(".drawingArea").
-append("svg:svg").
-attr("width", width).
-attr("height", height);
+var barChart = d3.select(".drawingArea")
+    .append("svg:svg")
+    .attr("width", width)
+    .attr("height", height);
 
-console.log("selected");
 
-barChart.selectAll("rect").
-data(data).
-enter().
-append("svg:rect").
-attr("x", function(d, index) { return index * (width / data.length); }).
-attr("y", function(d) { return height - d - 20; }).
-attr("height", function(d) { return d; }).
-attr("width", barWidth).
-attr("fill", function(d) { return "rgb(" + (d * 3) + ", 0, 0)"; });
+barChart.selectAll("rect")
+    .data(data)
+    .enter()
+    .append("svg:rect")
+    .attr("x", function(d, index) { return index * (width / data.length); })
+    .attr("y", function(d) { return height - d - 20; })
+    .attr("height", function(d) { return d; })
+    .attr("width", barWidth)
+    .attr("class", function(d, i) { return "element" + i})
+    .attr("fill", function(d) { return "rgb(" + (d * 3) + ", 0, 0)"; });
+
 
 barChart.selectAll("text")
     .data(data)
@@ -42,14 +49,12 @@ barChart.selectAll("text")
     .text(function(d) { return d; })
     .attr("x", function(d, index) { return index * (width / data.length) + barWidth / 4; })
     .attr("y", height )
-    .attr("width", barWidth);
+    .attr("width", barWidth)
+    .attr("class", function(d, i) { return "element" + i});
 
-
-//Test
-//Test call sorting algo
-function sort() {
-    insertionSort(data.slice());
-}
+$( document ).ready(function() {
+    initCode();
+});
 
 $(document).on("sort", function(event, newCommands) {
     console.log("sorted");
@@ -57,15 +62,25 @@ $(document).on("sort", function(event, newCommands) {
     cmd = newCommands.split('!');
 });
 
+function sort() {
+    insertionSort(data.slice());
+}
+
 function runCommand() {
         console.log(cmd[currentCmd]);
+        unHighlightCode();
         switch(cmd[currentCmd].substring(0, Math.min(cmd[currentCmd].length, 4))) {
             case "swap":
+                codeLineHighlight(6);
                 eval(cmd[currentCmd]);
-                redraw();
+                //redraw();
                 break;
             case "high":
+                codeLineHighlight(5);
                 clearHighlight();
+                eval(cmd[currentCmd]);
+                break;
+            case "code":
                 eval(cmd[currentCmd]);
                 break;
             default:
@@ -74,48 +89,84 @@ function runCommand() {
         currentCmd++;
 }
 
-function redraw() {
+/*
+function redraw(wait) {
     barChart.selectAll("rect").
     data(data).
     transition().
-    duration(1000).
+    delay(wait).
+    //duration(1000).
     attr("x", function(d, index) { return index * (width / data.length) + barWidth / 4; }).
     attr("y", function(d) { return height - d - 20; }).
     attr("height", function(d) { return d; });
     //attr("fill", function(d) { return "rgb(" + (d * 3) + ", 0, 0)"; });
 
+
+    /*
     barChart.selectAll("text")
         .data(data)
         .transition()
-        .duration(1000)
+        .delay(wait)
+        //.duration(1000)
         .text(function(d) { return d; })
         .attr("y", height );
-}
+        */
+//}
+
 
 function swap(a, b) {
-    var temp = data[a];
-    data[a] = data[b];
-    data[b] = temp;
+    var bar1 = barChart.selectAll("rect").filter(".element" + a);
+    var bar2 = barChart.selectAll("rect").filter(".element" + b);
+    var bar1Text = barChart.selectAll("text").filter(".element" + a);
+    var bar2Text = barChart.selectAll("text").filter(".element" + b);
+    var oldBar1_X = bar1.attr('x');
+    var oldBar1Text_X = bar1Text.attr('x');
+
+    bar1.attr("class", "element" + b)
+        .transition()
+        .duration(500)
+        .attr('x', bar2.attr('x'));
+    bar1Text.attr("class", "element" + b)
+        .transition()
+        .duration(500)
+        .attr('x', bar2Text.attr('x'));
+
+    bar2.attr("class", "element" + a)
+        .transition()
+        .duration(500)
+        .attr('x', oldBar1_X);
+    bar2Text.attr("class", "element" + a)
+        .transition()
+        .duration(500)
+        .attr('x', oldBar1Text_X);
 }
 
 function highlight(a, b, color) {
     color = color || "rgb(0, 255, 0)";
-    barChart.selectAll("rect").filter(":nth-child(" + (a+1) + ")")
+    barChart.selectAll("rect").filter(".element" + a)
         .transition()
         .duration(500)
-        .attr("fill", color);
-    barChart.selectAll("rect").filter(":nth-child(" + (b+1) + ")")
+        .attr("fill", "rgb(0,0,255)");
+    barChart.selectAll("rect").filter(".element" + b)
         .transition()
         .duration(500)
         .attr("fill", color);
 }
 
 function clearHighlight() {
-    barChart.selectAll("rect").
-    data(data).
-    transition().
-    duration(500).
-    attr("fill", function(d) { return "rgb(" + (d * 3) + ", 0, 0)"; });
+    barChart.selectAll("rect")
+        .data(data)
+        .transition()
+        .duration(500)
+        .attr("fill", function(d) { return "rgb(" + (d * 3) + ", 0, 0)"; });
+}
+
+function play() {
+    setTimeout(function(){
+        runCommand();
+        //Run next command if there is one
+        if(currentCmd < cmd.length) play();
+    }, 1000);
 }
 
 function initCode() {
@@ -123,14 +174,27 @@ function initCode() {
         .data(codeContent)
         .enter()
         .append("span")
-        .text(function (d) { return d });
+        .attr("id", function(d, i) { return "codeLine" + i})
+        .text(function (d) { return d })
+        .append("br");
+
+    //Restart code highlightning
+    hljs.initHighlighting.called = false;
+    hljs.initHighlighting();
 }
 
-function highlightCodeLine(lineNr) {
-    console.log(code);
-    //code.selectAll("span").filter(":nth-child(" + (lineNr) + ")")
-        //.transition()
-        //.duration(500)
-        //.style("background-color", "yellow");
+function codeLineHighlight(lineNr) {
+    //console.log(code);
+    code.selectAll("span").filter("#codeLine" + lineNr)
+        .transition()
+        .duration(500)
+        .style("background-color", "yellow");
+}
+
+function unHighlightCode() {
+    code.selectAll("span")
+        .transition()
+        .duration(500)
+        .style("background-color", "transparent");
 }
 
