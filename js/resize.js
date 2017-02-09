120b1811eccb426df72b0724e9b33ca09c8b90b1
Magnus Book <MS.Book@stud.uis.no>
2017-02-09 16:26:07 +0100

    Fixed setting view mode such that it shows the divided view for tablets

--- js/resize.js
+++ js/resize.js
@@ -3,14 +3,25 @@ var windowToggleState = 0;
 $(document).ready(function () {
     $("#graphics").resizable();
 
-    if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    /*if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
         windowToggleState = 1;
+        $("#graphics").width($("#view").width());
+        $("#code").width(0);
+    }*/
+
+    if (Math.min($(window).width(), $(window).height()) < 700) {
+        windowToggleState = 1;
+        $("#graphics").show();
         $("#graphics").width($("#view").width());
         $("#code").width(0);
     }
 
     $("#toggle-view-button").click(function () {
         if (++windowToggleState > 2) windowToggleState = 0;
+        if (windowToggleState === 0 && Math.min($(window).width(), $(window).height()) < 700) {
+            windowToggleState = 1;
+            $("#graphics").show();
+        }
 
         switch (windowToggleState) {
             case 0:
