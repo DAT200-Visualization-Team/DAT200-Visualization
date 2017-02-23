f7592869db4cd6d1f6ecc1de31822b46751215b2
Magnus Book <MS.Book@stud.uis.no>
2017-02-23 10:56:26 +0100

    Added ExpressionTree tests, removed some console logs and fixed some errors

--- js/algorithms/BubbleSort.js
+++ js/algorithms/BubbleSort.js
@@ -4,7 +4,6 @@ function bubbleSort(array) {
         newN = 0;
         for (var i = 1; i < n; i++) {
             if (array[i - 1] > array[i]) {
-                console.log("Swapping " + array[i-1] + " with " + array[i]);
                 var tmp = array[i];
                 array[i] = array[i - 1];
                 array[i - 1] = tmp;
