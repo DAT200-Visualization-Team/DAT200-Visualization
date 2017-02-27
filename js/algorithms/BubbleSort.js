2844cb1d0cf360e2c082ccc4cdab2cf09138d684
STranoey <s.tranoy@stud.uis.no>
2017-02-27 15:40:36 +0100

    bubble sort v1

--- js/algorithms/BubbleSort.js
+++ js/algorithms/BubbleSort.js
@@ -1,18 +1,24 @@
 function bubbleSort(array) {
+    var commands = [];
     var n = array.length;
     while(n > 0) {
-        newN = 0;
+        var newN = 0;
         for (var i = 1; i < n; i++) {
+            commands.push("highlight(" + (i-1) + ", " + i + ");");
             if (array[i - 1] > array[i]) {
-                console.log("Swapping " + array[i-1] + " with " + array[i]);
                 var tmp = array[i];
                 array[i] = array[i - 1];
                 array[i - 1] = tmp;
+                commands.push("swap(" + i + ", " + (i-1) + ");");
                 newN = i;
             }
         }
         n = newN;
+        commands.push("markAsSorted(" + newN + ");");
     }
+
+    console.log(commands);
+    $(document).trigger("sort", commands.join('!'));
 
     return array;
 }
