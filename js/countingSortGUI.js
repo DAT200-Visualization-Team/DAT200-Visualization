14c60cae5eeaaa8dab11c68498035d504df8d4be
Magnus Book <MS.Book@stud.uis.no>
2017-04-14 12:37:35 +0200

    Added command functionality for Fibonacci, insertion sort, counting sort and bubble sort. Also fixed svg size issue in counting sort

--- js/countingSortGUI.js
+++ js/countingSortGUI.js
@@ -1,9 +1,7 @@
 var data = [1, 2, 3, 4, 2, 1, 3, 3, 2, 1];
-//var data = [10, 20, 30, 40];
 
 var barWidth = 40;
 var width = (barWidth + 10) * data.length;
-//var width = 500;
 var height = 350;
 
 //Array size
@@ -50,121 +48,146 @@ var codeContent = [
     ];
 
 var code = d3.select("#code-text");
+
 var drawingArea = d3.select(".drawingArea")
-    .append("svg:svg")
-    .attr("width", width)
-    .attr("height", height);
-var unsortedArray = drawingArea.append("g");
+    .append("svg:svg");
 
-//Create unsorted array
-unsortedArray.selectAll("rect")
-    .data(data)
-    .enter()
-    .append("svg:rect")
-    .attr("x", function(d, index) { return index * (width / data.length); })
-    .attr("y", function(d) { return 0; })
-    .attr("height", arrElementHeight)
-    .attr("width", arrElementWidth - 5)
-    .attr("fill", "rgb(0, 127, 127)");
-
-unsortedArray.selectAll("text")
-    .data(data)
-    .enter()
-    .append("text")
-    .text(function(d) { return d; })
-    .attr("x", function(d, index) { return index * (width / data.length) + arrElementWidth / 3; })
-    .attr("y", 30)
-    .attr("width", barWidth);
-
-//Create counting array
-var countingArray = drawingArea.append("g");
-countingArray.selectAll("rect")
-    .data(countArray)
-    .enter()
-    .append("svg:rect")
-    .attr("x", function(d, index) { return index * (width / data.length); })
-    .attr("y", function(d) { return height / 3; })
-    .attr("height", arrElementHeight)
-    .attr("width", arrElementWidth - 5)
-    .attr("fill", "rgb(0, 127, 127)");
-
-countingArray.selectAll("text")
-    .data(countArray)
-    .enter()
-    .append("text")
-    .text(function(d) { return d; })
-    .attr("x", function(d, index) { return index * (width / data.length) + arrElementWidth / 3; })
-    .attr("y", height / 3 + 30)
-    .attr("width", barWidth);
-
-countingArray.selectAll("text.label")
-    .data(countArray)
-    .enter()
-    .append("text")
-    .text(function(d, index){ return index})
-    .attr("class", "label")
-    .attr("x", function(d, index) { return index * (width / data.length) + arrElementWidth / 3; })
-    .attr("y", height / 3 + 65)
-    .attr("width", barWidth);
-
-//Create sorted array
-var sortedArrayGUI = drawingArea.append("g");
-sortedArrayGUI.selectAll("rect")
-    .data(sortedArray)
-    .enter()
-    .append("svg:rect")
-    .attr("x", function(d, index) { return index * (width / data.length); })
-    .attr("y", function(d) { return 2 * height / 3; })
-    .attr("height", arrElementHeight)
-    .attr("width", arrElementWidth - 5)
-    .attr("fill", "rgb(0, 127, 127)");
-
-sortedArrayGUI.selectAll("text")
-    .data(sortedArray)
-    .enter()
-    .append("text")
-    .text(function(d) { return d; })
-    .attr("x", function(d, index) { return index * (width / data.length) + arrElementWidth / 3; })
-    .attr("y", 2 * height/3 + 30)
-    .attr("width", barWidth);
-
-//Create arrow
+var unsortedArray;
+var countingArray;
+var sortedArrayGUI;
 var arrowWidth = 10;
 var arrowHeight = 50;
 var arrowHeadHeight = 10;
-var arrow = drawingArea.append("svg")
-//    .attr("width", 19.2)
-//    .attr("height", 50)
-//    .attr("opacity", 0)
-    .attr('y', arrElementHeight)
-    .attr('x', arrElementWidth / 2 - arrowWidth);
+var arrow;
 
-//Arrowhead
-var arrowHead = arrow.append("path")
-    .attr("d", d3.symbol()
-        .type(d3.symbolTriangle)
-        .size(100))
-    .attr("stroke", "red")
-    .attr("fill", "rgb(255, 0,0)")
-    .attr("transform", "translate(" + arrowWidth + "," + arrowHeadHeight + ")" );
+createArray(data);
 
-//Line
-var line1 = arrow.append("line")
-    .attr("x1", arrowWidth)
-    .attr("y1", arrowHeadHeight)
-    .attr("x2", arrowWidth)
-    .attr("y2", arrowHeight)
-    .attr("stroke", "red")
-    .attr("stroke-width", "4");
+function createArray(array) {
+    $("svg").empty();
+    data = array;
 
+    //Max value in dataset
+    maxValue = Math.max.apply(null, data);
+    //Counting array
+    countArray = Array.apply(null, Array(maxValue + 1)).map(Number.prototype.valueOf, 0);
+    //Sorted array
+    sortedArray = Array.apply(null, Array(data.length)).map(String.prototype.valueOf, "");
+
+    width = (barWidth + 10) * data.length;
+
+    drawingArea.attr("width", (barWidth + 10) * Math.max(countArray.length, data.length)).attr("height", height);
+
+    unsortedArray = drawingArea.append("g");
+
+    //Create unsorted array
+    unsortedArray.selectAll("rect")
+        .data(data)
+        .enter()
+        .append("svg:rect")
+        .attr("x", function (d, index) { return index * (width / data.length); })
+        .attr("y", function (d) { return 0; })
+        .attr("height", arrElementHeight)
+        .attr("width", arrElementWidth - 5)
+        .attr("fill", "rgb(0, 127, 127)");
+
+    unsortedArray.selectAll("text")
+        .data(data)
+        .enter()
+        .append("text")
+        .text(function (d) { return d; })
+        .attr("x", function (d, index) { return index * (width / data.length) + arrElementWidth / 3; })
+        .attr("y", 30)
+        .attr("width", barWidth);
+
+    //Create counting array
+    countingArray = drawingArea.append("g");
+    countingArray.selectAll("rect")
+        .data(countArray)
+        .enter()
+        .append("svg:rect")
+        .attr("x", function (d, index) { return index * (width / data.length); })
+        .attr("y", function (d) { return height / 3; })
+        .attr("height", arrElementHeight)
+        .attr("width", arrElementWidth - 5)
+        .attr("fill", "rgb(0, 127, 127)");
+
+    countingArray.selectAll("text")
+        .data(countArray)
+        .enter()
+        .append("text")
+        .text(function (d) { return d; })
+        .attr("x", function (d, index) { return index * (width / data.length) + arrElementWidth / 3; })
+        .attr("y", height / 3 + 30)
+        .attr("width", barWidth);
+
+    countingArray.selectAll("text.label")
+        .data(countArray)
+        .enter()
+        .append("text")
+        .text(function (d, index) { return index })
+        .attr("class", "label")
+        .attr("x", function (d, index) { return index * (width / data.length) + arrElementWidth / 3; })
+        .attr("y", height / 3 + 65)
+        .attr("width", barWidth);
+
+    //Create sorted array
+    sortedArrayGUI = drawingArea.append("g");
+    sortedArrayGUI.selectAll("rect")
+        .data(sortedArray)
+        .enter()
+        .append("svg:rect")
+        .attr("x", function (d, index) { return index * (width / data.length); })
+        .attr("y", function (d) { return 2 * height / 3; })
+        .attr("height", arrElementHeight)
+        .attr("width", arrElementWidth - 5)
+        .attr("fill", "rgb(0, 127, 127)");
+
+    sortedArrayGUI.selectAll("text")
+        .data(sortedArray)
+        .enter()
+        .append("text")
+        .text(function (d) { return d; })
+        .attr("x", function (d, index) { return index * (width / data.length) + arrElementWidth / 3; })
+        .attr("y", 2 * height / 3 + 30)
+        .attr("width", barWidth);
+
+    //Create arrow
+    arrow = drawingArea.append("svg")
+        .attr('y', arrElementHeight)
+        .attr('x', arrElementWidth / 2 - arrowWidth);
+
+    //Arrowhead
+    var arrowHead = arrow.append("path")
+        .attr("d", d3.symbol()
+            .type(d3.symbolTriangle)
+            .size(100))
+        .attr("stroke", "red")
+        .attr("fill", "rgb(255, 0,0)")
+        .attr("transform", "translate(" + arrowWidth + "," + arrowHeadHeight + ")");
+
+    //Line
+    var line1 = arrow.append("line")
+        .attr("x1", arrowWidth)
+        .attr("y1", arrowHeadHeight)
+        .attr("x2", arrowWidth)
+        .attr("y2", arrowHeight)
+        .attr("stroke", "red")
+        .attr("stroke-width", "4");
+
+    cmd = [];
+    currentCmd = 0;
+}
+
+function startSorting() {
+    sort();
+    play();
+}
 
 $( document ).ready(function() {
     initCode();
 });
 
 $(document).on("sort", function(event, newCommands) {
-    console.log("sorted");
-    console.log(newCommands);
     cmd = newCommands.split('!');
 });
 
@@ -223,8 +246,6 @@ function pointArrow(index) {
     arrow.transition()
         .duration(500)
         .attr('x', index * (width / data.length) + arrElementWidth / 3 - arrowWidth / 2);
-        //.attr('y', array._groups[0][0].firstElementChild.attr('y'));
-    //console.log(array._groups[0][0].firstElementChild.attr('y'));
 }
 
 function highlight(index, array) {
@@ -245,24 +266,18 @@ function clearHighlight(array) {
 }
 
 function runCommand() {
-    console.log(cmd[currentCmd]);
     unHighlightCode();
     switch(cmd[currentCmd].substring(0, Math.min(cmd[currentCmd].length, 4))) {
         case "coun":
-            //showArrow();
             eval(cmd[currentCmd]);
             clearHighlight(unsortedArray);
-            //countUpdate();
             break;
         case "sort":
             //Hide arrow, no longer needed
-            //moveArrow(countingArray);
             eval(cmd[currentCmd]);
             hideArrow();
-            //sortedUpdate();
             break;
         case "high":
-            //clearHighlight();
             eval(cmd[currentCmd]);
             break;
         case "code":
@@ -300,7 +315,6 @@ function initCode() {
 }
 
 function codeLineHighlight(lineNr) {
-    //console.log(code);
     code.selectAll("span").filter("#codeLine" + lineNr)
         .transition()
         .duration(500)
