ba9c0df73d68af861e193f29e5207a47ad76bf85
danielbarati <daniel-barati@hotmail.com>
2017-04-17 14:04:53 +0200

    Some more preparations for AVL Tree visualization

--- js/visualization/AVLTreeVisualization.js
+++ js/visualization/AVLTreeVisualization.js
@@ -1,3 +1,53 @@
+var animationTime = 1000;
+var bst;
+var treeGUI;
+var loadingSequence;
+
+function makeGUIUnEditable() {
+    d3.select("#g_circles").selectAll('circle').on('click', null);
+    d3.select("#g_labels").selectAll('text').on('click', null);
+}
+
+function findNodeInTreeEditor(binaryNode) {
+    for(var i = 0; i < treeGUI.vis.length; i++) {
+        if (treeGUI.vis[i].l === binaryNode.element) return treeGUI.vis[i];
+    }
+}
+
+function findGUINodeByLabel(lbl) {}
+
+function getLeftGUIChild() {}
+
+function getRightGUIChild() {}
+
+function init(rootLbl) {
+    if(rootLbl == null) return;
+    bst = new AVLTree();
+    bst.insert(rootLbl);
+    treeGUI = new tree(rootLbl);
+
+    makeGUIUnEditable();
+}
+
+function visRotateWithLeftChild(k) {
+    /*k = findNodeInTreeEditor(k);
+    var kp = k.f;
+    var kl = kp.c*/
+
+}
+
+function visRotateWithRightChild(k) {
+    
+}
+
+function visDoubleRotateWithLeftChild() {
+
+}
+
+function visDoubleRotateWithRightChild() {
+
+}
+
 function visualizeInsert() {
 
 }
@@ -5,3 +55,6 @@ function visualizeInsert() {
 function visualizeRemove() {
 
 }
+
+
+init(50);
