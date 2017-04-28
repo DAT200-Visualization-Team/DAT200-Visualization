d7a2927f48c1ac2a44dee8892811bc3e0f78cad3
danielbarati <daniel-barati@hotmail.com>
2017-04-28 23:23:45 +0200

    Added some CSS-code

--- js/visualization/ArraySearchVisualization.js
+++ js/visualization/ArraySearchVisualization.js
@@ -5,15 +5,24 @@ function setArray(arr) {
     if(arr == null) {
         arr = [5,18,2,3,82,9,1,4,7];
     }
-    $("#arraySearchWrapper").empty();
+    $("#arraySearch").empty();
     for(var i = 0; i < arr.length; i++) {
-        $("#arraySearchWrapper").append('<div class="entry"><h4 class="entry-value noselect">' + arr[i] + '</h4></div>')
+        $("#arraySearch").append('<div class="entry"><h4 class="entry-value noselect">' + arr[i] + '</h4></div>')
     }
+    $("#arraySearch").append('<div class="arrow-up"></div>')
+
 }
 
 function visualizeLinearSearch(array, search) {
-    for (var i = 0; i < array.length; i++)
-        if (array[i] === search) return i;
+    for (var i = 0; i < array.length; i++) {
+        // TODO: move arrow
+
+        if (array[i] === search) {
+            // TODO: mark entry
+            return i;
+        }
+    }
+
 
     return -1;
 }
