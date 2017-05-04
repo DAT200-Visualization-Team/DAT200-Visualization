da4fcb1ae959ca2be3aececb008f6815c0d4b7d2
danielbarati <daniel-barati@hotmail.com>
2017-05-04 12:53:34 +0200

    Small changes in ArraySearch

--- js/visualization/ArraySearchVisualization.js
+++ js/visualization/ArraySearchVisualization.js
@@ -1,23 +1,24 @@
 var animationTime = 1000;
+var arr;
+var loadingSequence = [];
 
+function setArray(array) {
+    if(array == null) arr = [5,18,2,3,82,9,1,4,7];
+    else arr = array;
 
-function setArray(arr) {
-    if(arr == null) {
-        arr = [5,18,2,3,82,9,1,4,7];
-    }
     $("#arraySearch").empty();
     for(var i = 0; i < arr.length; i++) {
         $("#arraySearch").append('<div class="entry"><h4 class="entry-value noselect">' + arr[i] + '</h4></div>')
     }
-    $("#arraySearch").append('<div class="arrow-up"></div>')
+    $("#arraySearch").append('<div id="arrow-up"></div>')
 
 }
 
-function visualizeLinearSearch(array, search) {
-    for (var i = 0; i < array.length; i++) {
+function visualizeLinearSearch(search) {
+    for (var i = 0; i < arr.length; i++) {
         // TODO: move arrow
-
-        if (array[i] === search) {
+        loadingSequence.push({e: $("#arrow-up"), p: {translateX: "4rm"}, o: {duration: animationTime}})
+        if (arr[i] === search) {
             // TODO: mark entry
             return i;
         }
@@ -25,4 +26,8 @@ function visualizeLinearSearch(array, search) {
 
 
     return -1;
+}
+
+function animate() {
+    $.Velocity.RunSequence(loadingSequence);
 }
