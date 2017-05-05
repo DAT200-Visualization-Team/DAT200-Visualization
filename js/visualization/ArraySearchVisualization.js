51f00855abc91dab667e36446da21a4398dfedeb
danielbarati <daniel-barati@hotmail.com>
2017-05-05 14:15:51 +0200

    Added GSAP and illustrating its  awesomeness

--- js/visualization/ArraySearchVisualization.js
+++ js/visualization/ArraySearchVisualization.js
@@ -1,6 +1,6 @@
-var animationTime = 1000;
+var animationTime = 1;
 var arr;
-var loadingSequence = [];
+var tl = new TimelineMax();
 
 function setArray(array) {
     if(array == null) arr = [5,18,2,3,82,9,1,4,7];
@@ -10,24 +10,23 @@ function setArray(array) {
     for(var i = 0; i < arr.length; i++) {
         $("#arraySearch").append('<div class="entry"><h4 class="entry-value noselect">' + arr[i] + '</h4></div>')
     }
-    $("#arraySearch").append('<div id="arrow-up"></div>')
-
 }
 
 function visualizeLinearSearch(search) {
     for (var i = 0; i < arr.length; i++) {
-        // TODO: move arrow
-        loadingSequence.push({e: $("#arrow-up"), p: {translateX: "4rm"}, o: {duration: animationTime}})
+        if(i != 0) tl.to($("#arraySearch").children().eq(i - 1), animationTime, {backgroundColor: "#cc6c6c"});
+        tl.to($("#arraySearch").children().eq(i), animationTime, {backgroundColor: "#FFFF00"});
         if (arr[i] === search) {
-            // TODO: mark entry
+            tl.to($("#arraySearch").children().eq(i), animationTime, {backgroundColor: "green"});
             return i;
         }
     }
-
 
     return -1;
 }
 
 function animate() {
-    $.Velocity.RunSequence(loadingSequence);
+    setArray();
+    visualizeLinearSearch(9);
+    tl.play();
 }
