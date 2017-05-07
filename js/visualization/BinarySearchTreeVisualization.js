0eec9ead8dc4fcc3e59ad78b86c3f0d2124deb46
danielbarati <daniel-barati@hotmail.com>
2017-05-07 17:10:19 +0200

    Started changing BinarySearchTree visualization to support GSAP.

--- js/visualization/BinarySearchTreeVisualization.js
+++ js/visualization/BinarySearchTreeVisualization.js
@@ -2,6 +2,7 @@ var animationTime = 1000;
 var bst;
 var treeGUI;
 var loadingSequence;
+var codeDisplayManager;
 
 function makeGUIUnEditable() {
     d3.select("#g_circles").selectAll('circle').on('click', null);
@@ -9,13 +10,14 @@ function makeGUIUnEditable() {
 }
 
 function findNodeInTreeEditor(binaryNode) {
-    for(var i = 0; i < treeGUI.vis.length; i++) {
+    for (var i = 0; i < treeGUI.vis.length; i++) {
         if (treeGUI.vis[i].l === binaryNode.element) return treeGUI.vis[i];
     }
 }
 
 function init(rootLbl) {
-    if(rootLbl == null) return;
+    if (rootLbl == null) return;
+    codeDisplayManager = new CodeDisplayManager("javascript", "binarySearchTree");
     bst = new BinarySearchTree();
     bst.insert(rootLbl);
     treeGUI = new tree(rootLbl);
@@ -25,30 +27,41 @@ function init(rootLbl) {
 
 function visualizeInsert(lbl) {
     function insert(x, t) {
-        if(parent != bst.root) {
+        if (parent != bst.root) {
             var point = findNodeInTreeEditor(parent).p;
-            $("#g_lines line").each(function() {
-                if(point.x == $(this).attr("x2") && point.y == $(this).attr("y2")) {
-                    //console.log(point.x, $(this).attr("x2"));
-                    loadingSequence.push({e: $(this), p: {stroke: "#ffff00"}, o: {duration: animationTime}});
+            $("#g_lines line").each(function () {
+                if (point.x == $(this).attr("x2") && point.y == $(this).attr("y2")) {
+
+                    var lineToHighLight;
+                    if (direction == 'left') lineToHighLight = 3;
+                    else lineToHighLight = 5;
+                    var tmp = {e: $(this), p: {stroke: "#FFFF00"}, o: {duration: animationTime}};
+                    appendAnimation(lineToHighLight, [tmp], codeDisplayManager);
                 }
             });
         }
+        appendCodeLines([0], codeDisplayManager);
         if (t === null) {
             t = new BinaryNode(x);
-            if(parent != bst.root) {
-                loadingSequence[loadingSequence.length - 1].o.complete = function () {
+            if (parent != bst.root) {
+                var tmp = {e: "#treesvg", p: {}, o: {duration: animationTime}};
+                tmp.p.onComplete = function() {
                     parent = findNodeInTreeEditor(parent);
                     treeGUI.addLeaf(parent.v, direction, lbl);
-                    $("#g_lines line").each(function(i) {
+                    $("#g_lines line").each(function (i) {
                         $(this).removeAttr("style");
                         $(this).attr("stroke", "#808080");
                     });
                 };
+                appendAnimation(2, [tmp], codeDisplayManager);
             }
             else {
-                parent = findNodeInTreeEditor(parent);
-                treeGUI.addLeaf(parent.v, direction, lbl);
+                var tmp = {e: "#treesvg", p: {}, o: {duration: animationTime}};
+                tmp.p.onComplete = function() {
+                    parent = findNodeInTreeEditor(parent);
+                    treeGUI.addLeaf(parent.v, direction, lbl);
+                };
+                appendAnimation(2, [tmp], codeDisplayManager);
             }
         }
         else if (x - t.getElement() < 0) {
@@ -66,20 +79,20 @@ function visualizeInsert(lbl) {
         return t;
     }
 
-    loadingSequence = [];
+    codeDisplayManager.loadFunctions("insertNode");
+    codeDisplayManager.changeFunction("insertNode");
     var parent = bst.root, direction = null;
     insert(lbl, bst.root);
-    $.Velocity.RunSequence(loadingSequence);
     makeGUIUnEditable();
 }
 
 function visualizeSearch(lbl) {
     function search(x, t) {
         while (t !== null) {
-            if(parent != bst.root) {
+            if (parent != bst.root) {
                 var point = findNodeInTreeEditor(parent).p;
-                $("#g_lines line").each(function() {
-                    if(point.x == $(this).attr("x2") && point.y == $(this).attr("y2")) {
+                $("#g_lines line").each(function () {
+                    if (point.x == $(this).attr("x2") && point.y == $(this).attr("y2")) {
                         loadingSequence.push({e: $(this), p: {stroke: "#ffff00"}, o: {duration: animationTime}});
                     }
                 });
@@ -93,16 +106,16 @@ function visualizeSearch(lbl) {
                 t = t.getRight();
             }
             else {
-                if(t != bst.root) {
+                if (t != bst.root) {
                     var point = findNodeInTreeEditor(t).p;
-                    $("#g_lines line").each(function() {
-                        if(point.x == $(this).attr("x2") && point.y == $(this).attr("y2")) {
+                    $("#g_lines line").each(function () {
+                        if (point.x == $(this).attr("x2") && point.y == $(this).attr("y2")) {
                             loadingSequence.push({e: $(this), p: {stroke: "#ffff00"}, o: {duration: animationTime}});
                         }
                     });
                 }
-                $("#g_labels text").each(function() {
-                    if(t.element == $(this).text()) {
+                $("#g_labels text").each(function () {
+                    if (t.element == $(this).text()) {
                         d3.select("#treesvg").append('circle').attr('id', 'result')
                             .attr('cx', $(this).attr("x")).attr('cy', $(this).attr("y") - 5)
                             .attr('r', 13).attr('opacity', 0).style('fill', 'none').style('stroke', '#ffff00');
@@ -110,7 +123,7 @@ function visualizeSearch(lbl) {
 
                         loadingSequence[loadingSequence.length - 1].o.complete = function () {
                             $("#result").remove();
-                            $("#g_lines line").each(function(i) {
+                            $("#g_lines line").each(function (i) {
                                 $(this).removeAttr("style");
                                 $(this).attr("stroke", "#808080");
                             });
@@ -132,16 +145,17 @@ function visualizeSearch(lbl) {
 
 function visualizeRemove(lbl) {
     function remove(x, t) {
-        if(parent != bst.root) {
+        if (parent != bst.root) {
             var point = findNodeInTreeEditor(parent).p;
-            $("#g_lines line").each(function() {
-                if(point.x == $(this).attr("x2") && point.y == $(this).attr("y2")) {
+            $("#g_lines line").each(function () {
+                if (point.x == $(this).attr("x2") && point.y == $(this).attr("y2")) {
                     loadingSequence.push({e: $(this), p: {stroke: "#ffff00"}, o: {duration: animationTime}});
                 }
             });
         }
 
-        if (t === null) {}//TODO: give error
+        if (t === null) {
+        }//TODO: give error
         if (x - t.getElement() < 0) {
             parent = t, direction = 'left';
             t.setLeft(remove(x, t.getLeft()));
@@ -155,7 +169,7 @@ function visualizeRemove(lbl) {
             t.setRight(bst.removeMinNode(t.getRight())); // Fjerner noden den har bytta med, og setter
         }
         else {
-            if(t.getLeft() !== null) {
+            if (t.getLeft() !== null) {
                 parent = t, direction = 'left';
                 t = t.getLeft();
             }
@@ -176,8 +190,8 @@ function visualizeRemove(lbl) {
 
 init(50);
 /*visualizeInsert(70);
-visualizeInsert(60);
-visualizeInsert(80);
-visualizeInsert(30);
-visualizeInsert(20);
-visualizeInsert(40);*/
+ visualizeInsert(60);
+ visualizeInsert(80);
+ visualizeInsert(30);
+ visualizeInsert(20);
+ visualizeInsert(40);*/
