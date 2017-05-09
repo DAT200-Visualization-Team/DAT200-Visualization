823a89d6a6e20b0199f0a851543c09c7bd997149
Magnus Book <MS.Book@stud.uis.no>
2017-05-09 11:40:20 +0200

    Fixed bubble sort

--- js/bubbleSortGUI.js
+++ js/bubbleSortGUI.js
@@ -60,19 +60,26 @@ function swap(a, b) {
     var oldRectB_X = b * (width / data.length);
     var oldTextB_X = b * (width / data.length) + barWidth / 4;
 
+    var dxA = oldRectA_X - oldRectB_X;
+    var dxB = oldRectB_X - oldRectA_X;
+
 /*    tl//.addLabel('swap')
         .set(elementA.filter("rect"), {attr:{class: "element" + b}})
         .set(elementA.filter("text"), {attr:{class: "element" + b}})
         .set(elementB.filter("rect"), {attr:{class: "element" + a}})
         .set(elementB.filter("text"), {attr:{class: "element" + a}});*/
-    tl.to(elementA.filter("rect"), 0, {attr:{class: "element" + b}, ease:Linear.easeNone})
-        .to(elementA.filter("text"), 0, {attr:{class: "element" + b}, ease:Linear.easeNone})
-        .to(elementB.filter("rect"), 0, {attr:{class: "element" + a}, ease:Linear.easeNone})
-        .to(elementB.filter("text"), 0, {attr:{class: "element" + a}, ease:Linear.easeNone})
-        .to(elementA.filter("rect"), animationTime, {attr:{x:oldRectB_X}, ease:Linear.easeNone})
-        .to(elementA.filter("text"), animationTime, {attr:{x:oldTextB_X}, ease:Linear.easeNone}, '-=1')
-        .to(elementB.filter("rect"), animationTime, {attr:{x:oldRectA_X}, ease:Linear.easeNone}, '-=1')
-        .to(elementB.filter("text"), animationTime, {attr:{x:oldTextA_X}, ease:Linear.easeNone}, '-=1');
+    //tl.to(elementA.filter("rect"), 0, {attr:{class: "element" + b}, ease:Linear.easeNone})
+    //    .to(elementA.filter("text"), 0, {attr:{class: "element" + b}, ease:Linear.easeNone})
+    //    .to(elementB.filter("rect"), 0, {attr:{class: "element" + a}, ease:Linear.easeNone})
+    //    .to(elementB.filter("text"), 0, {attr:{class: "element" + a}, ease:Linear.easeNone})
+    //    .to(elementA.filter("rect"), animationTime, {attr:{x:oldRectB_X}, ease:Linear.easeNone})
+    //    .to(elementA.filter("text"), animationTime, {attr:{x:oldTextB_X}, ease:Linear.easeNone}, '-=1')
+    //    .to(elementB.filter("rect"), animationTime, {attr:{x:oldRectA_X}, ease:Linear.easeNone}, '-=1')
+    //    .to(elementB.filter("text"), animationTime, {attr:{x:oldTextA_X}, ease:Linear.easeNone}, '-=1');
+    tl.to(elementA, animationTime, { x: '-=' + dxA})
+        .to(elementB, animationTime, { x: '-=' + dxB }, '-=' + animationTime);
+    elementA.attr('class', 'element' + b);
+    elementB.attr('class', 'element' + a);
 }
 
 function startSorting() {
