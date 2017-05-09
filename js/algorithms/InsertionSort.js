f9aba53702df91fa7480ee8e97b686c5a2753825
STranoey <s.tranoy@stud.uis.no>
2017-05-09 17:13:45 +0200

    insertion sort

--- js/algorithms/InsertionSort.js
+++ js/algorithms/InsertionSort.js
@@ -1,28 +1,18 @@
 function insertionSort(a) {
-    //Commands for GUI
-    var commands = [];
 
     for(var p = 1; p < a.length; p++) {
-        commands.push("codeLineHighlight(2)");
         var tmp = a[p];
-        commands.push("codeLineHighlight(3)");
         var j = p;
-        commands.push("codeLineHighlight(4)");
 
-        for(; j > 0 && commands.push("highlight( " + j + ", " + (j - 1) + ")") && tmp < a[j - 1]; j--) {
+        for(; j > 0 && highlight(j, j-1) && tmp < a[j - 1]; j--) {
             a[j] = a[j - 1];
 
-            //GUI
-            commands.push("swap(" + j + ", " + (j - 1) + ");");
+            swap(j, j-1);
+            clearAllHighlight();
         }
         a[j] = tmp;
-        commands.push("codeLineHighlight(8)");
+        clearAllHighlight();
     }
-
-    commands.push("codeLineHighlight(10)");
-
-    //Send commands to GUI
-    $(document).trigger("sort", commands.join('!'));
 
     return a;
 }
