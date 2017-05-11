9f92bc4681e183465344af00f3ebdd7e688b62c3
Magnus Book <MS.Book@stud.uis.no>
2017-05-11 20:08:17 +0200

    All animations added to stepping system

--- js/algorithms/QuickSort.js
+++ js/algorithms/QuickSort.js
@@ -3,15 +3,16 @@ var size = 0;
 
 function QuickSort(arr) {
     this.a = arr;
-    this.medianOfThree = false;
     size = arr.length;
 }
 
-QuickSort.prototype.sort = function () {
-    this.quickSort(0, this.a.length - 1);
+QuickSort.prototype.sort = function (medianOfThree) {
+    if (medianOfThree)
+        this.quickSortMedian3(0, this.a.length - 1);
+    else
+        this.quickSortLeftPivot(0, this.a.length - 1);
 
     merge();
-    console.log(this.a);
     return this.a;
 };
 
@@ -19,13 +20,13 @@ QuickSort.prototype.quickSortLeftPivot = function (low, high) {
     if (low < high) {
         if (high - low < 4) {
             this.a = useInsertionSort(this.a, low, high + 1);
+        } else {
+            var p = this.partition1(low, high);
+            colorPartition(low, p, 'left');
+            this.quickSortLeftPivot(low, p);
+            colorPartition(p + 1, high, 'right');
+            this.quickSortLeftPivot(p + 1, high);
         }
-
-        var p = this.partition(low, high);
-        colorPartition(low, p, 'left');
-        this.quickSort(low, p);
-        colorPartition(p + 1, high, 'right');
-        this.quickSort(p + 1, high);
     }
 };
 
@@ -34,35 +35,32 @@ QuickSort.prototype.quickSortMedian3 = function (low, high) {
         if (high - low < 4) {
             this.a = useInsertionSort(this.a, low, high + 1);
         } else {
-
-            if (this.medianOfThree == true) {
-                var middle = Math.floor(parseInt(low + high) / 2);
-                if (this.a[middle] < this.a[low]) {
-                    this.a = swapReferences(this.a, low, middle);
-                    swap(low, middle);
-                }
-                if (this.a[high] < this.a[low]) {
-                    this.a = swapReferences(this.a, low, high);
-                    swap(low, high);
-                }
-                if (this.a[high] < this.a[middle]) {
-                    this.a = swapReferences(this.a, middle, high);
-                    swap(middle, high);
-                }
-                this.a = swapReferences(this.a, middle, high - 1);
-                swap(middle, high - 1);
+            var middle = Math.floor(parseInt(low + high) / 2);
+            if (this.a[middle] < this.a[low]) {
+                this.a = swapReferences(this.a, low, middle);
+                swap(low, middle);
             }
+            if (this.a[high] < this.a[low]) {
+                this.a = swapReferences(this.a, low, high);
+                swap(low, high);
+            }
+            if (this.a[high] < this.a[middle]) {
+                this.a = swapReferences(this.a, middle, high);
+                swap(middle, high);
+            }
+            this.a = swapReferences(this.a, middle, high - 1);
+            swap(middle, high - 1);
 
-            var p = this.partition(low, high);
+            var p = this.partition3(low, high);
             colorPartition(low, p, 'left');
-            this.quickSort(low, p);
+            this.quickSortMedian3(low, p);
             colorPartition(p + 1, high, 'right');
-            this.quickSort(p + 1, high);
+            this.quickSortMedian3(p + 1, high);
         }
     }
 };
 
-QuickSort.prototype.partition = function (low, high) {
+QuickSort.prototype.partition1 = function (low, high) {
     var pivot = this.a[high - 1];
 
     highlightPivot(low);
@@ -81,6 +79,27 @@ QuickSort.prototype.partition = function (low, high) {
         swap(i, j);
     }
 };
+
+QuickSort.prototype.partition3 = function (low, high) {
+    var pivot = this.a[high - 1];
+
+    highlightPivot(low);
+    markPivot(low, 'left');
+    markPivot(high, 'right');
+
+    var i, j;
+    for (i = low - 1, j = high + 1; ;) {
+        while (markPivot(i + 1, 'left') && this.a[++i] < pivot) {
+        }
+        while (markPivot(j - 1, 'right') && pivot < this.a[--j]) {
+        }
+        if (i >= j)
+            return j;
+        this.a = swapReferences(this.a, i, j);
+        swap(i, j);
+    }
+};
+
 
 function swapReferences(a, i, j) {
     var tmp = a[i];
--- js/quickSortGUI.js
+++ js/quickSortGUI.js
@@ -17,24 +17,10 @@ var textPositions = [];
 createRects(input);
 
 //Pivot marker left
-var pivotMarkerLeft = barChart.append("polygon")
-    .attr("fill", "yellow")
-    .attr("stroke",  "blue")
-    .attr("stroke-width", "2")
-    .attr("points", "05,30 15,10 25,30")
-    .attr("opacity", "0")
-    .attr("id", "pivotMarkerLeft");
-    //markPivot(0, "left");
+var pivotMarkerLeft;
 
 //Pivot marker Right
-var pivotMarkerRight = barChart.append("polygon")
-    .attr("fill", "yellow")
-    .attr("stroke",  "blue")
-    .attr("stroke-width", "2")
-    .attr("points", "05,30 15,10 25,30")
-    .attr("opacity", "0")
-    .attr("id", "pivotMarkerRight");
-    //markPivot(input.length-1, "right");
+var pivotMarkerRight;
 
 //Panning
 var pan = d3.zoom()
@@ -45,6 +31,9 @@ barChart.call(pan);
 var codeDisplayManager = new CodeDisplayManager('javascript', 'quickSort');
 
 function createRects(data) {
+    positions = [];
+    textPositions = [];
+
     barChart.selectAll("rect")
         .data(data)
         .enter()
@@ -65,7 +54,23 @@ function createRects(data) {
         .attr("x", function(d, index) { return index * (width / data.length) + barWidth / 4 + 100; })
         .attr("y", height - 30 )
         .attr("width", barWidth)
-        .attr("class", function(d, i) { return "element" + i});
+        .attr("class", function (d, i) { return "element" + i });
+
+    pivotMarkerLeft = barChart.append("polygon")
+        .attr("fill", "yellow")
+        .attr("stroke", "blue")
+        .attr("stroke-width", "2")
+        .attr("points", "05,30 15,10 25,30")
+        .attr("opacity", "0")
+        .attr("id", "pivotMarkerLeft");
+
+    pivotMarkerRight = barChart.append("polygon")
+        .attr("fill", "yellow")
+        .attr("stroke", "blue")
+        .attr("stroke-width", "2")
+        .attr("points", "05,30 15,10 25,30")
+        .attr("opacity", "0")
+        .attr("id", "pivotMarkerRight")
 
     for(var i = 0; i < data.length; i++) {
         positions.push([i * (width / data.length) + 100, height - (data[i]/Math.max.apply(null, data)) * 250 - 50]);
@@ -78,22 +83,25 @@ function panning() {
 }
 
 function startMedianOfThree() {
+    codeDisplayManager.loadFunctions('quickSortMedianOfThree', 'partition', 'insertionSort');
+    codeDisplayManager.changeFunction('quickSortMedianOfThree');
     sort(true);
 }
 
-function startSingleMedian() {
-    sort();
+function startMedianOfOne() {
+    codeDisplayManager.loadFunctions('quickSortLeftPivot', 'partition', 'insertionSort');
+    codeDisplayManager.changeFunction('quickSortLeftPivot');
+    sort(false);
 }
 
-var test;
-var sorted;
 function sort(median) {
-    test = new QuickSort(input.slice());
-    if(median == true) test.medianOfThree = true;
-    sorted = test.sort();
+    $('#barChart').empty();
+    createRects(input)
+    var qs = new QuickSort(input.slice());
+    qs.sort(median);
 }
 
-function markPivot(a, pivot) {
+function markPivot(a, pivot, line) {
     var text = barChart.selectAll("text").filter(".element" + a);
     var translation = getTranslate(text);
     
@@ -101,60 +109,39 @@ function markPivot(a, pivot) {
     var targetY = textPositions[a][1];
 
     if(pivot == 'left') {
-        tl.to($("#pivotMarkerLeft"), animationTime, {attr:{opacity: "1"}, x: targetX, y: targetY, ease:Linear.easeNone});
+        appendAnimation(line, [{ e: '#pivotMarkerLeft', p: { attr: { opacity: 1 }, x: targetX, y: targetY }, o: { duration: 1 } }], codeDisplayManager);
     } else {
-        tl.to($("#pivotMarkerRight"), animationTime, {attr:{opacity: "1"}, x: targetX, y: targetY, ease:Linear.easeNone});
+        appendAnimation(line, [{ e: '#pivotMarkerRight', p: { attr: { opacity: 1 }, x: targetX, y: targetY }, o: { duration: 1 } }], codeDisplayManager);
     }
     return true;
 }
 
-function colorElement(from, to, type) {
-    var selector = "";
-    for(var i = from; i <= to; i++) {
-        if(i != from) selector += ", ";
-        selector = selector + ".element" + i;
-    }
-    var elements = barChart.selectAll("rect").filter(selector);
-    if(type == "less") {
-        tl.to($(selector), animationTime, {attr:{fill:"magenta"}, ease:Linear.easeNone});
-    } else {
-        tl.to($(selector), animationTime, {attr:{fill:"lime"}, ease:Linear.easeNone});
-    }
-}
-
-function resetColor(from, to) {
-    var selector = "";
-    for(var i = from; i <= to; i++) {
-        if(i != from) selector += ", ";
-        selector = selector + ".element" + i;
-    }
-    barChart.selectAll("rect").filter(selector)
-        .attr("fill", "red");
-}
-
 function highlightPivot(index) {
     removeHighlightPivot();
-    tl.to($(".element" + index).filter("rect"), animationTime, {attr:{stroke:"green", "stroke-width":"5"}, ease:Linear.easeNone});
+    appendAnimation(null, [{ e: $('.element' + index).filter('rect'), p: { attr: {stroke:'green', 'stroke-width': 5}}, o: {duration:1}}], null);
 }
 
 function removeHighlightPivot() {
-    tl.to($("#barChart rect"), 0, {attr:{stroke:"none"}, ease:Linear.easeNone});
+    appendAnimation(null, [{ e: '#barChart rect', p: { attr: { stroke: 'none' } }, o: { duration: 1 } }], null);
 }
 
-function swap(a, b) {
+function swap(a, b, line) {
     var elementA = $(".element" + a);
     var elementB = $(".element" + b);
 
-    tl.to(elementA.filter("rect"), animationTime, {attr:{x: positions[b][0]}, ease:Linear.easeNone})
-        .to(elementA.filter("text"), animationTime, {attr:{x: positions[b][0] + barWidth/4}, ease:Linear.easeNone}, '-=' + animationTime)
-        .to(elementB.filter("rect"), animationTime, {attr:{x: positions[a][0]}, ease:Linear.easeNone}, '-=' + animationTime)
-        .to(elementB.filter("text"), animationTime, { attr: { x: positions[a][0] + barWidth / 4 }, ease: Linear.easeNone }, '-=' + animationTime);
+    appendAnimation(line, [
+        { e: elementA.filter("rect"), p: { attr: { x: positions[b][0] } }, o: { duration: 1 } },
+        { e: elementA.filter("text"), p: { attr: { x: positions[b][0] + barWidth / 4 } }, o: { duration: 1, position: '-=1' } },
+        { e: elementB.filter("rect"), p: { attr: { x: positions[a][0] } }, o: { duration: 1, position: '-=1' } },
+        { e: elementB.filter("text"), p: { attr: { x: positions[a][0] + barWidth / 4 } }, o: { duration: 1, position: '-=1' } }
+    ], codeDisplayManager);
 
     elementA.attr('class', 'element' + b);
     elementB.attr('class', 'element' + a);
 }
 
-function colorPartition(from, to, direction) {
+function colorPartition(from, to, direction, line) {
+    var animations = [];
 
     for(var i = from; i <= to; i++) {
         var element = d3.select(".element" + i);
@@ -162,26 +149,29 @@ function colorPartition(from, to, direction) {
         var y = parseInt(currentTranslation[1]);
         var x = parseInt(currentTranslation[0]);
         y = y + 50;
-        var test;
+        var delta;
 
         if(direction == "left") {
             x = x - 50/(y/50);
-            test = - 50/(y/50);
+            delta = -50 / (y / 50);
         } else if(direction == "right") {
             x = x + 50/(y/50);
-            test = 50/(y/50);
+            delta = 50 / (y / 50);
         }
 
-        textPositions[i][0] += test;
+        textPositions[i][0] += delta;
         textPositions[i][1] += 50;
-        tl.to($("#barChart .element" + i), animationTime, {x: '+=' + test, y: '+=50'}, 'partition');
+
+        animations.push({ e: $("#barChart .element" + i), p: { x: '+=' + delta, y: '+=50' }, o: { duration: 1, position: '-=1' } })
     }
+    animations[0].o.position = '+=0';
+    appendAnimation(line, animations, codeDisplayManager);
 
     hideArrows();
 }
 
 function hideArrows() {
-    tl.to($("#pivotMarkerLeft, #pivotMarkerRight"), animationTime, {attr:{opacity:"0"}, ease:Linear.easeNone}, '-=' + animationTime);
+    appendAnimation(null, [{ e: '#pivotMarkerLeft, #pivotMarkerRight', p: { attr: { opacity: "0" } }, o: { duration: 1, position: '-=1' } }], null);
 }
 
 function getTranslate(element) {
@@ -195,30 +185,31 @@ function getTranslate(element) {
     return [0, 0, 0, 0, 0, 0];
 }
 
-function merge() {
-    tl.to($("#barChart rect, #barChart text"), animationTime, {x:0, y:0, ease:Linear.easeNone});
+function merge(line) {
+    appendAnimation(line, [{ e: '#barChart rect, #barChart text', p: { x: 0, y: 0 }, o: { duration: 1 } }], codeDisplayManager);
 }
 
-function highlight(a, b, colorA, colorB) {
+function highlight(a, b, colorA, colorB, line) {
     colorA = colorA || "blue";
     colorB = colorB || "blue";
 
     var elementA = $(".element" + a).filter("rect");
     var elementB = $(".element" + b).filter("rect");
 
-    tl.addLabel('highlight')
-        .to(elementA.filter("rect"), animationTime, {attr:{fill: colorA}, ease:Linear.easeNone}, 'highlight')
-        .to(elementB.filter("rect"), animationTime, {attr:{fill: colorB}, ease:Linear.easeNone}, 'highlight');
+    appendAnimation(line, [
+        { e: elementA.filter("rect"), p: { attr: { fill: colorA } }, o: { duration: 1 } },
+        { e: elementB.filter("rect"), p: { attr: { fill: colorB } }, o: { duration: 1, position: '-=1' } }
+    ], codeDisplayManager);
 
     return true;
 }
 
 function clearAllHighlight() {
-    tl.to($("#barChart rect"), animationTime, {attr:{fill: "red"}, ease:Linear.easeNone});
+    appendAnimation(null, [{ e: '#barChart rect', p: { attr: { fill: "red" } }, o: { duration: 1 } }], null);
 }
 
 function clearHighlight(element, origColor) {
     origColor = origColor || "red";
-    tl.to($(".element" + element).filter("rect"), animationTime, {attr:{fill: origColor}, ease:Linear.easeNone});
+    appendAnimation(null, [{ e: $(".element" + element).filter("rect"), p: { attr: { fill: origColor } }, o: { duration: 1 } }], null);
 }
 
