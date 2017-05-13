dac55aedd3befa18b7ca004bb7d68662aa069f35
STranoey <s.tranoy@stud.uis.no>
2017-05-13 15:00:30 +0200

    mergesort!

--- js/algorithms/MergeSort.js
+++ js/algorithms/MergeSort.js
@@ -1,13 +1,10 @@
 // Merge sort
-var commands = [];
-
 function MergeSort(arr) {
     this.a = arr;
 }
 
 MergeSort.prototype.sort = function() {
     this.mergeSort(this.a, 0, this.a.length - 1);
-    $(document).trigger("sort", commands.join('!'));
     return this.a;
 };
 
@@ -16,15 +13,10 @@ MergeSort.prototype.mergeSort = function(a, left, right) {
         var center = parseInt((left + right) / 2);
 
         split(left, center, 'left');
-        commands.push("split(" + left + ", " + center + ", 'left');");
         this.mergeSort(this.a, left, center);
 
-
         split(center+1, right, 'right');
-        commands.push("split(" + right + "," + (center + 1) + ", 'right');");
         this.mergeSort(this.a, center + 1, right);
-
-
 
         this.merge(this.a, left, center + 1, right);
     }
@@ -37,23 +29,22 @@ MergeSort.prototype.merge = function(a, leftPos, rightPos, rightEnd) {
     var numElements = rightEnd - leftPos + 1;
 
     while(leftPos <= leftEnd && rightPos <= rightEnd) {
-        //TODO add highlighting to the values being compared
         if(this.a[leftPos] <= this.a[rightPos]) {
-            commands.push("merge(" + tmpPos + ", " + leftPos + ", 'left');");
+            merge(tmpPos, leftPos);
             tmpArray[tmpPos++] = this.a[leftPos++];
         } else {
-            commands.push("merge(" + tmpPos + ", " + rightPos + ", 'right');");
+            merge(tmpPos, rightPos);
             tmpArray[tmpPos++] = this.a[rightPos++];
         }
     }
 
     while(leftPos <= leftEnd) {
-        commands.push("merge(" + tmpPos + ", " + leftPos + ", 'left');");
+        merge(tmpPos, leftPos);
         tmpArray[tmpPos++] = this.a[leftPos++];
     }
 
     while(rightPos <= rightEnd) {
-        commands.push("merge(" + tmpPos + ", " + rightPos + ", 'right');");
+        merge(tmpPos, rightPos);
         tmpArray[tmpPos++] = this.a[rightPos++];
     }
 
@@ -61,13 +52,3 @@ MergeSort.prototype.merge = function(a, leftPos, rightPos, rightEnd) {
         a[rightEnd] = tmpArray[rightEnd];
     }
 };
-
-
-/*
-function MergeSort(arr) {
-    this.a = arr;
-    this.tmpArray = new Array(this.a.length);
-    this.sort(this.a, this.tmpArray, 0, this.a.length - 1);
-    return this.a;
-};
-*/
