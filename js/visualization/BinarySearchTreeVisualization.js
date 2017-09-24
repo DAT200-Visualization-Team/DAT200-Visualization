90b13ea0fe3674f986bcfeadaa1692d9b662568b
danielbarati <16683505+danielbarati@users.noreply.github.com>
2017-09-24 22:17:24 +0200

    Added visualization for FindNode in BinarySearchTree.

--- js/visualization/BinarySearchTreeVisualization.js
+++ js/visualization/BinarySearchTreeVisualization.js
@@ -15,6 +15,15 @@ function findNodeInTreeEditor(binaryNode) {
     }
 }
 
+function createMarker() {
+    d3.select("#treesvg").append('g').attr('class', 'markers')
+        .append('circle').attr('id', 'marker').attr('cx', 0).attr('cy', 0).attr('r', 14).attr('opacity', 0);
+}
+
+function moveMarker(cx, cy) {
+    return [{ e: $("#marker"), p: { x: cx, y: cy, opacity: 1 }, o: { duration: animationTime} }];
+}
+
 function init(rootLbl) {
     if (rootLbl == null) return;
     codeDisplayManager = new CodeDisplayManager("javascript", "binarySearchTree");
@@ -26,11 +35,25 @@ function init(rootLbl) {
 }
 
 function visualizeInsert(lbl) {
+    function highlightNodeEdge(n, dir) {
+        var point = findNodeInTreeEditor(n).p;
+        $("#g_lines line").each(function () {
+            if (point.x == $(this).attr("x2") && point.y == $(this).attr("y2")) {
+
+                var lineToHighLight;
+                if (dir == 'left') lineToHighLight = 3;
+                else lineToHighLight = 5;
+                var tmp = {e: $(this), p: {stroke: "#FFFF00"}, o: {duration: animationTime}};
+                appendAnimation(lineToHighLight, [tmp], codeDisplayManager);
+            }
+        });
+    }
+
     function insert(x, t) {
         var hasNotBeenInIf = true;
         var highlightedEdges = [];
 
-        if (parent != bst.root) {
+        /*if (parent != bst.root) {
             var point = findNodeInTreeEditor(parent).p;
             $("#g_lines line").each(function () {
                 if (point.x == $(this).attr("x2") && point.y == $(this).attr("y2")) {
@@ -42,7 +65,7 @@ function visualizeInsert(lbl) {
                     appendAnimation(lineToHighLight, [tmp], codeDisplayManager);
                 }
             });
-        }
+        }*/
 
         appendCodeLines([0], codeDisplayManager);
         if (t === null && hasNotBeenInIf) {
@@ -88,24 +111,23 @@ function visualizeInsert(lbl) {
             }
         }
 
-        appendCodeLines([2], codeDisplayManager);
-        if (x - t.getElement() < 0 && hasNotBeenInIf) {
+        else if (x - t.getElement() < 0 && hasNotBeenInIf) {
             hasNotBeenInIf = false;
             parent = t, direction = 'left';
             if (parent == bst.root) appendCodeLines([3], codeDisplayManager);
+            highlightNodeEdge(parent, 'left')
             t.setLeft(insert(x, t.getLeft()));
         }
 
-        appendCodeLines([4], codeDisplayManager);
-        if (x - t.getElement() > 0 && hasNotBeenInIf) {
+        else if (x - t.getElement() > 0 && hasNotBeenInIf) {
             hasNotBeenInIf = false;
             parent = t, direction = 'right';
             if (parent == bst.root) appendCodeLines([5], codeDisplayManager);
+            highlightNodeEdge(parent, 'right')
             t.setRight(insert(x, t.getRight()));
         }
 
-        appendCodeLines([6], codeDisplayManager);
-        if(hasNotBeenInIf) {
+        else if(hasNotBeenInIf) {
             hasNotBeenInIf = false;
             //TODO: visualize error
             //throw { name: "DuplicateItemException", message: "Duplicate items are not allowed" };
@@ -122,59 +144,53 @@ function visualizeInsert(lbl) {
 }
 
 function visualizeSearch(lbl) {
-    function search(x, t) {
-        while (t !== null) {
-            if (parent != bst.root) {
-                var point = findNodeInTreeEditor(parent).p;
-                $("#g_lines line").each(function () {
-                    if (point.x == $(this).attr("x2") && point.y == $(this).attr("y2")) {
-                        loadingSequence.push({e: $(this), p: {stroke: "#ffff00"}, o: {duration: animationTime}});
-                    }
-                });
-            }
-            if (x - t.getElement() < 0) {
-                parent = t, direction = 'left';
-                t = t.getLeft();
-            }
-            else if (x - t.getElement() > 0) {
-                parent = t, direction = 'right';
-                t = t.getRight();
-            }
-            else {
-                if (t != bst.root) {
-                    var point = findNodeInTreeEditor(t).p;
-                    $("#g_lines line").each(function () {
-                        if (point.x == $(this).attr("x2") && point.y == $(this).attr("y2")) {
-                            loadingSequence.push({e: $(this), p: {stroke: "#ffff00"}, o: {duration: animationTime}});
-                        }
-                    });
-                }
-                $("#g_labels text").each(function () {
-                    if (t.element == $(this).text()) {
-                        d3.select("#treesvg").append('circle').attr('id', 'result')
-                            .attr('cx', $(this).attr("x")).attr('cy', $(this).attr("y") - 5)
-                            .attr('r', 13).attr('opacity', 0).style('fill', 'none').style('stroke', '#ffff00');
-                        loadingSequence.push({e: $("#result"), p: {opacity: 1}, o: {duration: animationTime}});
+    function highlightNodeEdge(n, dir) {
+        var point = findNodeInTreeEditor(n).p;
+        $("#g_lines line").each(function () {
+            if (point.x == $(this).attr("x2") && point.y == $(this).attr("y2")) {
 
-                        loadingSequence[loadingSequence.length - 1].o.complete = function () {
-                            $("#result").remove();
-                            $("#g_lines line").each(function (i) {
-                                $(this).removeAttr("style");
-                                $(this).attr("stroke", "#808080");
-                            });
-                        };
-                    }
-                });
-
-                return;
+                var lineToHighLight;
+                if (dir == 'left') lineToHighLight = 2;
+                else lineToHighLight = 4;
+                var tmp = {e: $(this), p: {stroke: "#FF0000"}, o: {duration: animationTime}};
+                appendAnimation(lineToHighLight, [tmp], codeDisplayManager);
             }
-        }
+        });
     }
 
-    loadingSequence = [];
-    var parent = bst.root, direction = null;
-    search(lbl, bst.root);
-    $.Velocity.RunSequence(loadingSequence);
+    codeDisplayManager.loadFunctions("findNode");
+    codeDisplayManager.changeFunction("findNode");
+    var t = bst.root;
+    appendCodeLines([0], codeDisplayManager);
+    while (t !== null) {
+        if (lbl - t.getElement() < 0) {
+            appendCodeLines([1], codeDisplayManager);
+            t = t.getLeft();
+            if (t !== null) highlightNodeEdge(t, 'left');
+
+        }
+        else if (lbl - t.getElement() > 0) {
+            appendCodeLines([1], codeDisplayManager);
+            t = t.getRight();
+            if (t !== null) highlightNodeEdge(t, 'right');
+
+        }
+        else {
+            appendCodeLines([5], codeDisplayManager);
+            appendCodeLines([6], codeDisplayManager);
+            // TODO: show node
+
+            return t; // Match
+            appendCodeLines([7, 0], codeDisplayManager);
+        }
+
+    }
+
+    // TODO: show fail
+    appendCodeLines([9], codeDisplayManager);
+    return null; // Not Found
+
+
     makeGUIUnEditable();
 }
 
@@ -216,11 +232,17 @@ function visualizeRemove(lbl) {
         return t;
     }
 
-    loadingSequence = [];
+    codeDisplayManager.loadFunctions("removeNode");
+    codeDisplayManager.changeFunction("removeNode");
     var parent = bst.root, direction = null;
     remove(lbl, bst.root);
-    $.Velocity.RunSequence(loadingSequence);
     makeGUIUnEditable();
 }
 
 init(50);
+setTimeout(function(){
+    visualizeInsert(90);
+    setTimeout(function() {
+        visualizeInsert(30);
+    }, 3000)
+}, 3000);
