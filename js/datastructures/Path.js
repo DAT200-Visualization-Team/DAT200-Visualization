3cf7278cf551cea927dbaf806819944be5989234
magnu <MS.Book@stud.uis.no>
2017-12-12 15:42:05 +0100

    Added toString function for vertices and paths

--- js/datastructures/Path.js
+++ js/datastructures/Path.js
@@ -9,3 +9,7 @@ Path.prototype.compareTo = function(rhs) {
     var otherCost = rhs.cost;
     return this.cost < otherCost ? - 1 : this.cost > otherCost ? 1 : 0;
 }
+
+Path.prototype.varString = function () {
+    "Path: <br> &emsp; dest: " + this.dest.varString() + "<br> &emsp; cost: " + this.cost;
+}
